3acb40127921a81bca7a7fcebdff87b455355d6d
Yunochi <yuno@yunochi.com>
2024-11-14 12:55:18 +0900

    Fix: 질문 생성 API 취약점 수정

--- src/app/api/db/create-question/route.ts
+++ src/app/api/db/create-question/route.ts
@@ -9,22 +9,16 @@ export async function POST(req: NextRequest) {
 
   try {
     const body: CreateQuestionDto = await req.json();
-    const newQuestion = await prisma.question.create({
-      data: {
-        question: body.question,
-        questioner: body.questioner,
-        questioneeHandle: body.questionee,
-      },
-    });
-
     const questionee_user = await prisma.user.findUnique({
       where: {
         handle: body.questionee,
       },
     });
+
     if (!questionee_user) {
       throw new Error(`questionee_user not found`);
     }
+    // 제시된 questioner 핸들이 JWT토큰의 핸들과 일치하는지 검사
     if (body.questioner) {
       const token = req.cookies.get('jwtToken')?.value;
       try {
@@ -37,25 +31,38 @@ export async function POST(req: NextRequest) {
         }
       } catch (err) {
         console.log(`questioner verify ERROR! ${err}`);
+        throw(err);
       }
     }
+
+    //질문 생성
+    const newQuestion = await prisma.question.create({
+      data: {
+        question: body.question,
+        questioner: body.questioner,
+        questioneeHandle: body.questionee,
+      },
+    });
   
+    //알림 전송
     const url = `${process.env.WEB_URL}/main/questions`;
-    sendNotify(questionee_user, newQuestion.question, url);
+    setImmediate(() => {
+      sendNotify(questionee_user, newQuestion.question, url);
+    });
     
-    // notify send 기다라지 않고 202반환
-    return NextResponse.json({ status: 202 });
+    // notify send 기다라지 않고 200반환
+    return NextResponse.json({ status: 200 });
   } catch (err) {
-    console.log(err);
+    return NextResponse.json(`Error! ${err}`, { status: 500 });
   }
-  return NextResponse.json({ status: 500 });
 }
 
 
 async function sendNotify(questionee: user, question: string, url: string): Promise<void> {
   const notify_host = process.env.NOTI_HOST;
+  console.log(`try to send notification to ${questionee.handle}`);
   try {
-    await fetch(`https://${notify_host}/api/notes/create`, {
+    const res = await fetch(`https://${notify_host}/api/notes/create`, {
       method: "POST",
       headers: {
         authorization: `Bearer ${process.env.NOTI_TOKEN}`,
@@ -67,8 +74,11 @@ async function sendNotify(questionee: user, question: string, url: string): Prom
         text: `${questionee.handle} <네오-퀘스돈> 새로운 질문이에요!\nQ. ${question}\n ${url}`,
       }),
     });
+    if (res.status !== 200){
+      throw new Error(`Note create error`);
+    }
   }
   catch (error) {
-    console.warn('Post-question: fail to send notify: ', error);
+    console.error('Post-question: fail to send notify: ', error);
   }
 }
