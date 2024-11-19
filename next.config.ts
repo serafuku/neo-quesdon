139972ac508d149ffa1979476f0df69e9c7fe5ce
Squarecat-meow <yozumina@serafuku.moe>
2024-11-19 10:02:19 +0900

    🔧 reactStrictMode 다시 true로 설정

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,9 @@ import "reflect-metadata";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  reactStrictMode: true,
   experimental: {
     forceSwcTransforms: true,
-  }
+  },
 };
 
 export default nextConfig;
