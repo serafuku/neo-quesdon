57a49454b0fb1fc2b8368a63258d43b9ac4ecc80
Yunochi <yuno@yunochi.com>
2024-12-02 13:49:39 +0900

    환경변수 설정 안된 경우 에러 뱉도록

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,5 +2,12 @@ export async function register() {
   if (process.env.NODE_ENV === 'production') {
     const qs = await import('@/api/_queue-service/queueService');
     qs.QueueService.get();
+
+    const env_arr = ['REDIS_HOST', 'REDIS_PORT', 'WEB_URL', 'DATABASE_URL', 'JWT_SECRET', 'NOTI_TOKEN', 'NOTI_HOST'];
+    env_arr.forEach((key) => {
+      if (!process.env[key]) {
+        throw new Error(`ENV ${key} are not set`);
+      }
+    });
   }
 }
