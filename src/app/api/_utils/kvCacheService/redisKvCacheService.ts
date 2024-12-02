8c718b5baf7defdccb481b67b378a75ada25d561
Yunochi <yuno@yunochi.com>
2024-12-02 18:07:00 +0900

    WIP: Redis 캐시 서비스

---
+++ src/app/api/_utils/kvCacheService/redisKvCacheService.ts
@@ -0,0 +1,48 @@
+import { Logger } from '@/utils/logger/Logger';
+import { Redis } from 'ioredis';
+
+export class RedisKvCacheService {
+  private static instance: RedisKvCacheService;
+  private redis: Redis;
+  private logger = new Logger('RedisKvCacheService');
+  private constructor() {
+    const host = process.env.REDIS_HOST;
+    const port_str = process.env.REDIS_PORT;
+    if (typeof host !== 'string' || typeof port_str !== 'string') {
+      throw new Error('Redis host or port not provided!');
+    }
+    const port = Number.parseInt(port_str);
+    this.redis = new Redis({
+      host: host,
+      port: port,
+    });
+  }
+
+  public static getInstance() {
+    if (!RedisKvCacheService.instance) {
+      RedisKvCacheService.instance = new RedisKvCacheService();
+    }
+    return RedisKvCacheService.instance;
+  }
+  public async get<T>(
+    fn: () => Promise<T>,
+    cacheConfig: { key: string; ttl: number },
+    noCache: boolean = false,
+  ): Promise<T> {
+    const { key, ttl } = cacheConfig;
+    const cached_data = await this.redis.get('cache:' + key);
+    if (cached_data === null || noCache) {
+      const data = await fn();
+      const res = await this.redis.setex('cache:' + key, ttl, JSON.stringify(data));
+      if (res !== 'OK') {
+        this.logger.error('Redis Cache Save Fail!');
+        throw new Error('Redis Save Fail');
+      }
+      this.logger.debug(`Return data without cache (Cache-Miss or Bypass) Key: ${key}, data: ${JSON.stringify(data)}`);
+      return data;
+    } else {
+      this.logger.debug(`Return data with cache (Cache-Hit) Key: ${key}, data: ${cached_data}`);
+      return JSON.parse(cached_data) as T;
+    }
+  }
+}
