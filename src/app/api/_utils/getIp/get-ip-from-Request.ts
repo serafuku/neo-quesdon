b00ef0fddafd83fcced5ef80668d176422a53460
Yunochi <yuno@yunochi.com>
2024-12-12 13:37:13 +0900

    Refactor: WebsocketService 리팩토링
    - 응답 없는경우 연결해제
    - 코드 정리

--- src/app/api/_utils/getIp/get-ip-from-Request.ts
+++ src/app/api/_utils/getIp/get-ip-from-Request.ts
@@ -1,9 +1,28 @@
-import { NextRequest } from 'next/server';
 import { Logger } from '@/utils/logger/Logger';
+import { IncomingMessage } from 'http';
+import { NextRequest } from 'next/server';
 
 const logger = new Logger('get-ip-from-request');
+export function getIpFromIncomingMessage(req: IncomingMessage) {
+  const x_forward_header = req.headers['x-forwarded-for'];
+  if (typeof x_forward_header !== 'string') {
+    logger.debug(`x-forwarded-for is not string`);
+    return '127.0.0.1';
+  }
+  const ip = x_forward_header.split(',')[0].replaceAll(' ', '');
+  if (!ip) {
+    logger.warn('IP detect fail!');
+  }
+  return ip ?? '127.1.2.3';
+}
+
 export function getIpFromRequest(req: NextRequest) {
-  const ip = req.headers.get('x-forwarded-for')?.split(',')[0].replace(' ', '');
+  const x_forward_header = req.headers.get('x-forwarded-for');
+  if (typeof x_forward_header !== 'string') {
+    logger.debug(`x-forwarded-for is not string`);
+    return '127.0.0.1';
+  }
+  const ip = x_forward_header.split(',')[0].replaceAll(' ', '');
   if (!ip) {
     logger.warn('IP detect fail!');
   }
