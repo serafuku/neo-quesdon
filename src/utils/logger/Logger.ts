7a621a1186697c6e4797d3aa9120959f81f9d34b
Yunochi <yuno@yunochi.com>
2024-12-12 11:04:11 +0900

    로그 레벨 오버라이드 가능하게 변경

--- src/utils/logger/Logger.ts
+++ src/utils/logger/Logger.ts
@@ -1,16 +1,50 @@
 type options = {
   noColor?: boolean;
 };
+
+const logLevels = {
+  debug: 0,
+  info: 1,
+  warn: 2,
+  error: 3,
+} as const;
+
+type logLevelNameType = keyof typeof logLevels;
+type logLevelNumType = (typeof logLevels)[logLevelNameType];
 export class Logger {
   private name: string;
   private options: options;
+  private logLevel: logLevelNumType;
   constructor(name: string, loggerOptions?: options) {
     this.name = name;
     this.options = loggerOptions ?? {};
+    if (process.env.APP_LOG_LEVEL) {
+      const levelName = process.env.APP_LOG_LEVEL.toLowerCase();
+      switch (levelName) {
+        case 'verbose':
+          this.logLevel = logLevels['debug'];
+          break;
+        case 'debug':
+        case 'info':
+        case 'warn':
+        case 'error':
+          this.logLevel = logLevels[levelName];
+          break;
+        case 'log':
+        default:
+          this.logLevel = logLevels['info'];
+      }
+    } else {
+      if (process.env.NODE_ENV === 'production') {
+        this.logLevel = logLevels['info'];
+      } else {
+        this.logLevel = logLevels['debug'];
+      }
+    }
   }
 
   public debug(...args: unknown[]) {
-    if (process.env.NODE_ENV === 'production') {
+    if (this.logLevel > logLevels['debug']) {
       return;
     }
     const time = Math.floor(Date.now() / 1000);
@@ -22,6 +56,9 @@ export class Logger {
   }
 
   public log(...args: unknown[]) {
+    if (this.logLevel > logLevels['info']) {
+      return;
+    }
     const time = Math.floor(Date.now() / 1000);
     if (this.options.noColor) {
       console.log(`[${time}] [${this.name}] INFO: `, ...args);
@@ -31,6 +68,9 @@ export class Logger {
   }
 
   public warn(...args: unknown[]) {
+    if (this.logLevel > logLevels['warn']) {
+      return;
+    }
     const time = Math.floor(Date.now() / 1000);
     if (this.options.noColor) {
       console.log(`[${time}] [${this.name}] WARN: `, ...args);
