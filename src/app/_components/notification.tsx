40c7a780e6698571bcbd78d2c0bd2b50703c63b9
Squarecat-meow <yozumina@serafuku.moe>
2024-12-18 22:39:17 +0900

    ✨ 알림창 최종구현

--- src/app/_components/notification.tsx
+++ src/app/_components/notification.tsx
@@ -3,27 +3,32 @@
 import { NotificationPayloadTypes } from '@/app/_dto/notification/notification.dto';
 import { useContext, useEffect } from 'react';
 import { NotificationContext } from '../main/layout';
+import Link from 'next/link';
 
 export default function Notification() {
   const notificationContext = useContext(NotificationContext);
 
-  const parseNoti = (noti: NotificationPayloadTypes) => {
+  const parseNoti = (noti: NotificationPayloadTypes, i: number) => {
+    const isRead = i + 1 > (notificationContext?.unread_count ?? 0) ? true : false;
     switch (noti.notification_name) {
       case 'answer_on_my_question': {
         return (
-          <div
-            key={noti.data?.id}
-            className="flex items-center gap-2 my-2 p-2 desktop:p-4 w-full rounded-box glass shadow"
-          >
-            <img
-              src={noti.data?.answeredPerson?.avatarUrl}
-              alt="answered persen avatar"
-              className="w-16 h-16 rounded-full"
-            />
-            <div className="flex flex-col">
-              <span className="font-thin italic">&quot;{noti.data?.question}&quot;</span>
-              <p>{noti.data?.answer}</p>
-            </div>
+          <div key={noti.data?.id}>
+            <Link href={`/main/user/${noti.data.answeredPersonHandle}/${noti.data.id}`} replace>
+              <div
+                className={`flex items-center gap-2 my-2 p-2 relative desktop:p-4 w-full rounded-box shadow ${isRead ? 'bg-gray-100 dark:bg-gray-700' : 'bg-base-100 dark:bg-slate-500'}`}
+              >
+                <img
+                  src={noti.data?.answeredPerson?.avatarUrl}
+                  alt="answered persen avatar"
+                  className={`w-16 h-16 rounded-full ${isRead && 'opacity-70'}`}
+                />
+                <div className={`flex flex-col ${isRead && 'text-slate-500 dark:text-slate-400'}`}>
+                  <span className="font-thin italic">&quot;{noti.data?.question}&quot;</span>
+                  <p>{noti.data?.answer}</p>
+                </div>
+              </div>
+            </Link>
           </div>
         );
       }
@@ -50,7 +55,7 @@ export default function Notification() {
       <h2 className="text-3xl desktop:text-4xl font-semibold">
         {notificationContext && notificationContext.unread_count}개의 새로운 알림
       </h2>
-      {notificationContext?.notifications && notificationContext.notifications.map((el) => parseNoti(el))}
+      {notificationContext?.notifications && notificationContext.notifications.map((el, i) => parseNoti(el, i))}
     </div>
   );
 }
