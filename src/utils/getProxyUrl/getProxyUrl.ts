927e7f2f200175ee35a7009bad5ea92dbc4812db
Yuno <yuno@yunochi.com>
2024-12-21 00:47:37 +0900

    🐛 getProxyUrl 수정

--- src/utils/getProxyUrl/getProxyUrl.ts
+++ src/utils/getProxyUrl/getProxyUrl.ts
@@ -1,7 +1,3 @@
 export function getProxyUrl(url: string) {
-  const baseUrl = process.env.WEB_URL;
-  if (!baseUrl) {
-    return url;
-  }
-  return `${baseUrl}/api/web/proxy?url=${encodeURIComponent(url)}`;
+  return `/api/web/proxy?url=${encodeURIComponent(url)}`;
 }
