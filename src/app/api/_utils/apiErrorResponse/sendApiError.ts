72e14139b6df1402bcfcca81c6bae8077ccd93a2
Yunochi <yuno@yunochi.com>
2024-12-31 15:11:03 +0900

    🎨 ApiErrorResponseDto 로 ApiError 응답 표준화

--- src/app/api/_utils/apiErrorResponse/sendApiError.ts
+++ src/app/api/_utils/apiErrorResponse/sendApiError.ts
@@ -1,6 +1,11 @@
-import { ApiErrorTypes } from '@/app/_dto/apiErrorTypes';
+import { ApiErrorResponseDto } from '@/app/_dto/api-error/api-error.dto';
+import { ApiErrorTypes } from '@/app/_dto/api-error/apiErrorTypes';
 import { NextResponse } from 'next/server';
 
 export function sendApiError(code: number, message: string | string[], errorType: ApiErrorTypes) {
-  return NextResponse.json({ error_type: errorType, message: message }, { status: code });
+  const errorRes: ApiErrorResponseDto = {
+    error_type: errorType,
+    message: message,
+  };
+  return NextResponse.json(errorRes, { status: code });
 }
